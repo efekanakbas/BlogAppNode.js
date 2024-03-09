bcadcc264235d2516fe727a8c2ee3a5ba14b9b2e
Efekan Akbaş <efekanakbas98@gmail.com>
2024-03-10 02:48:21 +0300

    try3

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
-const express = require("express");
+const express = require('express')
+
 const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const routerFeed = require("./routes/feed");
-const routerAuth = require("./routes/auth");
-// require("dotenv").config();
+app.use(express.json())
 
-// Mongo DB Connections
-// mongoose
-//   .connect(process.env.MONGODB_URI)
-//   .then(() => {
-//     console.log("MongoDB Connection Succeeded.");
-//   })
-//   .catch((error) => {
-//     console.log("Error in DB connection: " + error);
-//   });
-
-// Middleware Connections
-// app.use(cors());
-app.use(express.json({ limit: "30mb", extended: true }));
-// app.use(express.urlencoded({ limit: "30mb", extended: true }));
-// app.use('/uploads', express.static('uploads'))
-
-// Routes
-// app.use('/', routerFeed)
-// app.use('/', routerAuth)
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    })
+app.get('/', (req,res) => {
+    res.send('Hello Wordld!!!!!!')
 })
 
-// Connection
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("App running in port: " + PORT);
-});
+app.listen(process.env.PORT || 5000, () => {
+    console.log("server")
+})
