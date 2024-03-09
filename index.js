9415a073e2f89d0dd4f96f94fecf74dbc5315cc0
Efekan Akbaş <efekanakbas98@gmail.com>
2024-03-10 02:38:25 +0300

    herokuUpdate

--- index.js
+++ index.js
@@ -1,48 +1,38 @@
-    const express = require("express");
-    const app = express();
-    const cors = require("cors");
-    const mongoose = require("mongoose");
-    const routerFeed = require("./routes/feed");
-    const routerAuth = require("./routes/auth");
-    const https = require('https');
-    const fs = require('fs');
-    require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const routerFeed = require("./routes/feed");
+const routerAuth = require("./routes/auth");
+require("dotenv").config();
 
-    // License options
+// Mongo DB Connections
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB Connection Succeeded.");
+  })
+  .catch((error) => {
+    console.log("Error in DB connection: " + error);
+  });
 
-    const options = {
-        key: fs.readFileSync('server.key'),
-        cert: fs.readFileSync('server.crt'),
-    };
-    const server = https.createServer(options, app);
+// Middleware Connections
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use('/uploads', express.static('uploads'))
 
-    // Mongo DB Connections
-    mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("MongoDB Connection Succeeded.");
+// Routes
+app.use('/', routerFeed)
+app.use('/', routerAuth)
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
     })
-    .catch((error) => {
-        console.log("Error in DB connection: " + error);
-    });
+})
 
-    // Middleware Connections
-    app.use(cors());
-    app.use(express.json({ limit: "30mb", extended: true }));
-    app.use(express.urlencoded({ limit: "30mb", extended: true }));
-    app.use('/uploads', express.static('uploads'))
-
-    // Routes
-    app.use('/', routerFeed)
-    app.use('/', routerAuth)
-    app.get('/', (req, res) => {
-        res.json({
-            message: 'Hello World!'
-        })
-    })
-
-    // Connection
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, '0.0.0.0', () => {
-    console.log("App running in port: " + PORT);
-    });
+// Connection
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("App running in port: " + PORT);
+});
