f6f7836bef8658ad2a14fe728095bfc8eb1dee85
Efekan Akbaş <efekanakbas98@gmail.com>
2024-03-11 15:35:56 +0300

    patch

--- routes/feed.js
+++ routes/feed.js
@@ -5,6 +5,6 @@ const uploadMiddleware = require("../middleware/uploadMiddleware");
 const authenticateJWT = require('../middleware/authenticateJWT');
 
 router.get('/feeds', authenticateJWT , feedGET)
-router.post('/feeds', authenticateJWT ,  feedPOST)
+router.post('/feeds', authenticateJWT , uploadMiddleware,  feedPOST)
 
 module.exports = router;
